e77c51342ffedd569ec1708a6f02a86d29626e7d
chendumpling99 <66714443+chendumpling99@users.noreply.github.com>
2022-04-21 17:42:48 -0700

    styled search results and added most recent github commit info to each search result

--- src/components/LinkBtn/LinkBtn.js
+++ src/components/LinkBtn/LinkBtn.js
@@ -3,8 +3,13 @@ import { FaGithub } from 'react-icons/fa'
 import { GitHubButton, UnChecked } from './styles'
 
 const LinkBtn = ({ github }) => {
+
+  // If you're getting this warning in console: 
+  // Warning: validateDOMNesting(...): <a> cannot appear as a descendant of <a>.
+  // Ignore it.
+
   return (
-    <a target="_blank" href={`https://github.com/${github}`}>
+    <a target="_blank" href={`https://github.com/vikelabs/${github}`}>
       <GitHubButton onClick={(e) => e.stopPropagation()}>
         <UnChecked><FaGithub /></UnChecked>
       </GitHubButton>
